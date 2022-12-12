2c17420d9b115b64bf5d989b3e8b880e43826d0f
wi2410 <101503219+wi2410@users.noreply.github.com>
2022-12-13 01:58:50 +0200

    upd selectors

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,2 +1,4 @@
-export const getContacts = state => state.contacts.contacts;
-export const getFilterInput = state => state.filter;
+export const selectContacts = state => state.contacts.contacts;
+export const selectFilterInput = state => state.filter;
+export const selectIsLoading = state => state.contacts.isLoading;
+export const selectError = state => state.contacts.error;

bcb14a05c29a159b775305420a5c3cc8e9a9bc24
wi2410 <101503219+wi2410@users.noreply.github.com>
2022-12-13 01:51:43 +0200

    upd store + add operations

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistReducer, persistStore } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import thunk from 'redux-thunk';
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
 
-const persistConfig = {
-  key: 'root',
-  storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, contactsReducer);
-
 export const store = configureStore({
   reducer: {
-    contacts: persistedReducer,
+    contacts: contactsReducer,
     filter: filterReducer,
   },
-  middleware: [thunk],
 });
-
-export const persistor = persistStore(store);
